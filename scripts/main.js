eec85daa4435649ba688a4eca646e075f061b2e6
Mirek Rup <miroslawrup7@gmail.com>
2022-04-24 16:25:24 +0200

    lightbox 2

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@
 
 const lightbox = document.querySelector("#lightbox");
 const imgWrapper = document.querySelector(".img-wrapper");
+const titleWrapper = document.querySelector(".img-wrapper > .img-title");
 const closeBtn = lightbox.querySelector(".close-btn");
 const gallery = document.querySelector(".gallery-box");
 const images = gallery.querySelectorAll("img");
@@ -10,15 +11,25 @@ images.forEach((image) => {
     image.addEventListener("click", (e) => {
         lightbox.classList.add("active");
         const img = document.createElement("img");
-        console.log(image.src);
         img.src = image.src;
+
+        const filenameExt = image.src.substring(image.src.lastIndexOf("/") + 1);
+        const filename = filenameExt.slice(0, filenameExt.length - 4);
+        console.log(filename);
+        const title = document.createElement("p");
+        title.innerText = filename;
 
         if (imgWrapper.querySelector(".img-wrapper > img")) {
             imgWrapper.removeChild(
                 imgWrapper.querySelector(".img-wrapper > img")
             );
         }
-        imgWrapper.appendChild(img);
+        imgWrapper.prepend(img);
+
+        if (titleWrapper.querySelector("p")) {
+            titleWrapper.removeChild(titleWrapper.querySelector("p"));
+        }
+        titleWrapper.appendChild(title);
     });
 });
 
